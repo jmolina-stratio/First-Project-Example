3e148059b95fb6b25cb8abfe823bf6520c7c81b2
jmolina <jmolina@stratio.com>
2022-05-04 11:06:56 +0200

    Primera versión

---
+++ src/main/scala/org/example/App.scala
@@ -0,0 +1,15 @@
+package org.example
+
+import scala.Console.println
+
+
+/**
+ * @author ${user.name}
+ */
+object App {
+  
+  def main(args : Array[String]) {
+    println( "Hello World!" )
+  }
+
+}
